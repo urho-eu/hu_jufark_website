03c03b9531412fa6fc89a1d64efb0e47a4be3a79
Ferenc Szekely <ferenc.szekely@gmail.com>
2011-10-23 19:44:17 +0300

    child nodes, workflows and attachement variant definitions; slight refactor in injector

--- injector.php
+++ injector.php
@@ -2,10 +2,12 @@
 class hu_juhfark_website_injector
 {
     var $mvc = null;
+    var $component = 'hu_juhfark_website';
 
     public function __construct()
     {
         $this->mvc = midgardmvc_core::get_instance();
+        $this->component = $this->mvc->configuration->nodes['component'];
     }
 
     /**
@@ -14,20 +16,19 @@ class hu_juhfark_website_injector
     public function inject_template(midgardmvc_core_request $request)
     {
         // We inject the template to provide the juhfark.hu styling
-        $request->add_component_to_chain($this->mvc->component->get('hu_juhfark_website'), true);
+        $request->add_component_to_chain($this->mvc->component->get($this->component), true);
 
         // set the page title
         $title = $this->mvc->configuration->site_name;
 
         $node = $request->get_node();
 
-        if ($node->get_parent_node())
+        if (   is_object($node)
+            && $node->get_parent_node())
         {
-            $title .= ' - ' . $this->mvc->i18n->get($this->mvc->configuration->nodes['children'][$node->name]['title'], $node->get_component());
+            $title .= ' - ' . $this->mvc->i18n->get($this->mvc->configuration->nodes['children'][$node->name]['title'], $this->component);
         }
         $this->mvc->head->set_title($title);
-
-
     }
 }
 ?>
